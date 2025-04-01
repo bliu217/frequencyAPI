271918a51b1cb4583d9456a212f6b0e9e07d395d
UBC Student <bliu41@students.cs.ubc.ca>
2025-03-31 23:33:54 -0700

    First Commit

---
+++ backend/src/main/java/org/frequency/frequencyapi/FrequencyApiApplication.java
@@ -0,0 +1,13 @@
+package org.frequency.frequencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequencyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequencyApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/frequency/frequencyapi/FrequencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.frequency.frequencyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequencyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
